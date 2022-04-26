4dbf007fb82ba6e32e4843c425e835cd8cfd9ea9
doru84 <49203894+doru84@users.noreply.github.com>
2022-04-26 18:26:15 +0200

    Initial commit

---
+++ src/main/java/Account.java
@@ -0,0 +1,24 @@
+public class Account {
+    private String name;
+    private String surname;
+    private String accNumber;
+    private long balance;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getSurname(){
+        return surname = surname;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+        public String getAccNumber(){
+                return accNumber;
+        }
+        public void setAccNumber(String accNumber){
+            this.accNumber = accNumber;
+
+}
