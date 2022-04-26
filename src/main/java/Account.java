0e26f3211a4719728c30508499b1286feb59d64d
doru84 <49203894+doru84@users.noreply.github.com>
2022-04-26 18:49:54 +0200

    add new method

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class Account {
     private String name;
     private String surname;
     private String accNumber;
     private long balance;
+    Scanner scanner = new Scanner(System.in);
 
     public String getName(){
         return name;
@@ -13,12 +16,30 @@ public class Account {
     public String getSurname(){
         return surname = surname;
     }
-    public void setSurname(String surname){
+    public void setSurname(String surname) {
         this.surname = surname;
+    }
         public String getAccNumber(){
                 return accNumber;
         }
-        public void setAccNumber(String accNumber){
+        public void setAccNumber(String accNumber) {
             this.accNumber = accNumber;
+        }
+            public long getBalance(){
+        return balance;
+            }
+            public void setBalance (long balance){
+        this.balance = balance;
+            }
+            public void createAccount(){
+                System.out.println("Enter your name");
+                name = scanner.next();
+                System.out.println("Enter your surname");
+                surname = scanner.next();
+                System.out.println("Enter your account number");
+                accNumber = scanner.next();
+                System.out.println("Enter your balance");
+                balance = scanner.nextLong();
+            }
 
 }
