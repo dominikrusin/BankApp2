ce1d810465867cc8d42639a0224c645b89000586
doru84 <49203894+doru84@users.noreply.github.com>
2022-04-26 20:14:18 +0200

    add new method: add withdrawal

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -7,51 +7,71 @@ public class Account {
     private long balance;
     Scanner scanner = new Scanner(System.in);
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    public String getSurname(){
+
+    public String getSurname() {
         return surname = surname;
     }
+
     public void setSurname(String surname) {
         this.surname = surname;
     }
-        public String getAccNumber(){
-                return accNumber;
-        }
-        public void setAccNumber(String accNumber) {
-            this.accNumber = accNumber;
-        }
-            public long getBalance(){
+
+    public String getAccNumber() {
+        return accNumber;
+    }
+
+    public void setAccNumber(String accNumber) {
+        this.accNumber = accNumber;
+    }
+
+    public long getBalance() {
         return balance;
-            }
-            public void setBalance (long balance){
+    }
+
+    public void setBalance(long balance) {
         this.balance = balance;
-            }
-            public void createAccount(){
-                System.out.println("Enter your name");
-                name = scanner.next();
-                System.out.println("Enter your surname");
-                surname = scanner.next();
-                System.out.println("Enter your account number");
-                accNumber = scanner.next();
-                System.out.println("Enter your balance");
-                balance = scanner.nextLong();
-            }
+    }
+
+    public void createAccount() {
+        System.out.println("Enter your name");
+        name = scanner.next();
+        System.out.println("Enter your surname");
+        surname = scanner.next();
+        System.out.println("Enter your account number");
+        accNumber = scanner.next();
+        System.out.println("Enter your balance");
+        balance = scanner.nextLong();
+    }
 
     public void showAccount() {
         System.out.println("Name: " + name);
-        System.out.println("Surname :"+surname);
-        System.out.println("Account number :"+accNumber);
-        System.out.println("Balance :"+balance);
+        System.out.println("Surname :" + surname);
+        System.out.println("Account number :" + accNumber);
+        System.out.println("Balance :" + balance);
     }
-    public void depositeMoney(){
+
+    public void depositeMoney() {
         System.out.println("How much You want to deposite?: ");
         long amount = scanner.nextLong();
         balance = balance + amount;
         System.out.println("Your balance ic: " + balance);
+    }
+
+    public void withdrawal() {
+        System.out.println("How much money you want to withdrawal?: ");
+        long amount = scanner.nextLong();
+        if (balance >= amount) {
+            balance = balance - amount;
+            System.out.println("Your current balance is:" + balance);
+        } else {
+            System.out.println("Your balance is less then " + amount);
+        }
     }
 }
--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -8,15 +8,18 @@ public class BankApp {
     account.createAccount();
         System.out.println("Hello in our bank");
         System.out.println("1. Display account details");
+        System.out.println("2. Deposite money");
+        System.out.println("3. Withdrawal money");
         System.out.println("Enter your choice");
         int choice ;
         choice = scanner.nextInt();
         switch (choice) {
             case 1:
                 account.showAccount();
-
             case 2:
                 account.depositeMoney();
+            case 3:
+                account.withdrawal();
 
         }
     }
