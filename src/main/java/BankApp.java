6a7d6b10834594c7fefa6c82ecc526008dda2cc1
doru84 <49203894+doru84@users.noreply.github.com>
2022-04-26 20:35:47 +0200

    add new method: add do while

--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -2,25 +2,38 @@ import java.util.Scanner;
 
 public class BankApp {
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-
-    Account account = new Account();
-    account.createAccount();
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello in our bank");
-        System.out.println("1. Display account details");
-        System.out.println("2. Deposite money");
-        System.out.println("3. Withdrawal money");
-        System.out.println("Enter your choice");
-        int choice ;
-        choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                account.showAccount();
-            case 2:
-                account.depositeMoney();
-            case 3:
-                account.withdrawal();
+        Account account = new Account();
+        account.createAccount();
 
+
+        int choice;
+        do {
+            System.out.println("1. Display account details");
+            System.out.println("2. Deposite money");
+            System.out.println("3. Withdrawal money");
+            System.out.println("4. Exit app");
+            System.out.println("Enter your choice");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    account.showAccount();
+                    break;
+                case 2:
+                    account.depositeMoney();
+                    break;
+                case 3:
+                    account.withdrawal();
+                    break;
+                case 4:
+                    System.out.println("By by");
+                    break;
+
+            }
         }
+        while (choice != 4);
+
+
     }
 }
